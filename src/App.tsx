e589c4ea9351fca4b40a03ee7c089dabf1f869a8
LuuSohn <luuhoangson2002@gmail.com>
2023-07-14 14:57:31 +0700

    tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 
-
 function App() {
   
   return (
-    <div className="App">
-     
+    <div className="App ">
+        <h1 className=" flex justify-center">
+        <div>01</div>
+        <div>02</div>
+        <div>03</div>
+        </h1>    
     </div>
   )
 }
